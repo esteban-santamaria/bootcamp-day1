1199dfada1368b4a1fa17cb384b625c34b654e30
Esteban Santamaria <esteban.santamaria@yuxpacific.com>
2018-02-05 19:36:57 -0500

    add docs in cli.json, change href and upload first mission

---
+++ src/app/my-component/my-component.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+	selector: 'app-my-component',
+	templateUrl: './my-component.component.html',
+	styleUrls: [ './my-component.component.css' ]
+})
+export class MyComponentComponent {
+	title = 'my application';
+	author = 'Esteban Restrepo';
+	count = 0;
+	imgSource = './../favicon.ico';
+	disable = true;
+}
